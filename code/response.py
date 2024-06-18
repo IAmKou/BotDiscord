b83c1d0a9e7475f4f20c692e669adef7788e0881
IAmKou <luongcu111@gmail.com>
2024-06-18 23:08:13 +0700

    upup

--- code/response.py
+++ code/response.py
@@ -5,4 +5,7 @@ def get_response(user_input: str) -> str:
     
     if lowered == 'nep':
         return 'The gay one ?'
+    elif lowered == '<:noinho:1247579809647693826>':
+        return 'Is that nep gay ?'
+    
 
