415628d7cd9e2bf8b5428cefbcc2c030a50599e1
NeBrat123 <152869595+NeBrat123@users.noreply.github.com>
2024-06-18 22:13:15 +0700

    Update response.py

--- code/response.py
+++ code/response.py
@@ -5,6 +5,6 @@ def get_response(user_input: str) -> str:
     
     if lowered == 'nep':
         return 'The gay one ?'
-    elif lowered == ':noinho:':
-        return 'Emoji này gợi nhớ mị lúc Nep Gay thật...giờ vẫn thế' 
+    if lowered == ':noinho:':
+        return 'Emoji này gợi nhớ mị lúc Nep gay thật...giờ vẫn thế' 
 
